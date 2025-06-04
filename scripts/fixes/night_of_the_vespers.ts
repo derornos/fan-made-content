84f8639fe800628d09f948e0815ddc4fe796c7df
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2025-06-04 22:48:52 +0200

    Add Night of the Vespers

---
+++ scripts/fixes/night_of_the_vespers.ts
@@ -0,0 +1,114 @@
+[
+  [
+    "0c5f0495-c084-4c8f-966c-4638549a3df2",
+    "trait:miskatonic, trait:medic, trait:scholar",
+  ],
+  [
+    "10d2a3b0-4003-4c57-bae1-2a13810d8600",
+    "trait:agency, trait:criminal, trait:detective, trait:police",
+  ],
+  [
+    "118f8a3c-f00e-4962-bdab-92bc732d27aa",
+    "trait:clairvoyant, trait:drifter, trait:sorcerer",
+  ],
+  [
+    "17a51fdd-ca11-439e-bf49-051d04869860",
+    "trait:clairvoyant, trait:drifter, trait:wayfarer",
+  ],
+  [
+    "18d84fc6-985c-4d22-be68-2a7b628f3428",
+    "trait:criminal, trait:drifter, trait:wayfarer",
+  ],
+  [
+    "221d61f7-20f6-4333-8b0c-c927f0ba571a",
+    "trait:blessed, trait:chosen, trait:sorcerer",
+  ],
+  [
+    "86831b0b-38d5-44ee-84bb-a0a74fde5df6",
+    "trait:criminal, trait:miskatonic, trait:silver twilight",
+  ],
+  [
+    "f88c718b-f00b-427f-af99-330377d24477",
+    "investigator:94be00ee-821c-46df-b051-40a2b38e501d",
+  ],
+  [
+    "5caf1688-1fb4-4771-832d-08def15a1895",
+    "investigator:94be00ee-821c-46df-b051-40a2b38e501d",
+  ],
+  [
+    "02d9a66a-6a69-4818-923e-86d55904fb48",
+    "investigator:316ddf18-cebf-4908-9778-140c98cc6243",
+  ],
+  [
+    "b11e53c6-3d05-45c0-b848-a05b13147e04",
+    "investigator:316ddf18-cebf-4908-9778-140c98cc6243",
+  ],
+  [
+    "16a04764-1118-4037-9cdd-7307e6a3e31f",
+    "investigator:1030fde5-0c2f-4ced-a29f-5b61dd32c51b",
+  ],
+  [
+    "d90738fa-8ba2-4a1b-8d0d-fbfbde83c574",
+    "investigator:1030fde5-0c2f-4ced-a29f-5b61dd32c51b",
+  ],
+  [
+    "05f6b498-d068-4c15-a227-cfd493930bdc",
+    "investigator:00e54a05-edc1-4f18-86e4-2ceafeee215f",
+  ],
+  [
+    "080c839e-6084-469b-b54e-4c94b8f7d37e",
+    "investigator:00e54a05-edc1-4f18-86e4-2ceafeee215f",
+  ],
+  [
+    "67c26999-509c-4b2b-851e-f98f7936e8d1",
+    "investigator:00e54a05-edc1-4f18-86e4-2ceafeee215f",
+  ],
+  [
+    "0d09f239-2548-4669-9379-85c3d5dcc388",
+    "investigator:00e54a05-edc1-4f18-86e4-2ceafeee215f",
+  ],
+  [
+    "8b94fbd4-bc36-48c9-84cd-76fb210306be",
+    "investigator:00e54a05-edc1-4f18-86e4-2ceafeee215f",
+  ],
+  [
+    "a2fc482a-964c-4514-8bd3-3f15f07c6a21",
+    "investigator:00e54a05-edc1-4f18-86e4-2ceafeee215f",
+  ],
+  [
+    "a3ba4fcc-992a-42ef-8c32-01bcf248b150",
+    "investigator:00e54a05-edc1-4f18-86e4-2ceafeee215f",
+  ],
+  [
+    "02a039e3-c777-498b-b504-d349f3766456",
+    "investigator:7b4d6148-1078-4d41-94e1-adce7d5c42f2",
+  ],
+  [
+    "8c3c05cd-030b-438c-b9aa-9ba7999771db",
+    "investigator:7b4d6148-1078-4d41-94e1-adce7d5c42f2",
+  ],
+  [
+    "b64eccd4-6cc1-4b19-95dd-78f0d254e4e1",
+    "trait:artist, trait:blessed, trait:drifter, trait:scholar",
+  ],
+  [
+    "b1e4efc3-0a89-4510-b256-be66fa1e7201",
+    "trait:chosen, trait:dreamer, trait:sorcerer, trait:warden",
+  ],
+  [
+    "263f82df-f519-43aa-b6dc-90e1741edc1a",
+    "trait:artist, trait:dreamer, trait:performer",
+  ],
+  [
+    "1c7c51ee-a298-4462-b335-0c99c25fef70",
+    "trait:cultist, trait:cursed, trait:scholar, trait:sorcerer",
+  ],
+  [
+    "f7be1a8a-24b9-48f0-9013-cff883049e95",
+    "trait:agency, trait:criminal, trait:miskatonic, trait:medic",
+  ],
+  [
+    "f9545c2d-a3c3-412c-8482-bb4a70278e76",
+    "trait:artist, trait:blessed, trait:drifter, trait:warden",
+  ],
+];
